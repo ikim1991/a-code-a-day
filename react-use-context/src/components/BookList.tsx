975849448e8994d4653f227e1608cffa2e1421d0
Chris K <ikim1991@gmail.com>
2020-12-16 11:35:05 -0500

    React Use Context using Hooks

--- react-use-context/src/components/BookList.tsx
+++ react-use-context/src/components/BookList.tsx
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component{
-    static contextType = ThemeContext;
-    render(){
+// class BookList extends Component{
+//     static contextType = ThemeContext;
+//     render(){
 
-        const { isLightTheme, light, dark } = this.context;
-        const theme= isLightTheme ? light : dark;
-        return(
-            <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
-                <ul>
-                    <li style={{background: theme.ui}}>The way of the kings</li>
-                    <li style={{background: theme.ui}}>The name of the wind</li>
-                    <li style={{background: theme.ui}}>The final empire</li>
-                </ul>
-            </div>
-        )
-    }
+//         const { isLightTheme, light, dark } = this.context;
+//         const theme= isLightTheme ? light : dark;
+//         return(
+        //     <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
+        //         <ul>
+        //             <li style={{background: theme.ui}}>The way of the kings</li>
+        //             <li style={{background: theme.ui}}>The name of the wind</li>
+        //             <li style={{background: theme.ui}}>The final empire</li>
+        //         </ul>
+        //     </div>
+        // )
+//     }
+// }
+
+const BookList = () => {
+
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = (isLightTheme) ? light : dark;
+
+    const { books } = useContext(BookContext)
+
+    return(
+        <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
+            <ul>
+                {books.map((book: any) => {
+                    return (
+                        <li style={{background: theme.ui}} key={book.id}>{book.title}</li>
+                    )
+                })}
+            </ul>
+        </div>
+)
 }
 
 export default BookList;
